67aa75abba041663ea4e8869358f4a67f9e0430b
wangxiaonan <wangxiaonan.master@bytedance.com>
2020-03-18 16:07:13 +0800

    绑定数据

--- app/src/main/java/com/bytedance/i18n/daydayup/MainActivity.kt
+++ app/src/main/java/com/bytedance/i18n/daydayup/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bytedance.i18n.daydayup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mv_info.bindData("杨子骥的成长","8.0")
     }
 }
--- app/src/main/java/com/bytedance/i18n/daydayup/MovieInfoView.java
+++ app/src/main/java/com/bytedance/i18n/daydayup/MovieInfoView.java
@@ -2,22 +2,39 @@ package com.bytedance.i18n.daydayup;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * @author wangxiaonan
  */
 public class MovieInfoView extends RelativeLayout {
+
+    ImageView coverView;
+    TextView movieName;
+    TextView movieScore;
+
     public MovieInfoView(Context context) {
-        this(context,null);
+        this(context, null);
     }
 
     public MovieInfoView(Context context, AttributeSet attrs) {
-        this(context, attrs,0);
+        this(context, attrs, 0);
     }
 
     public MovieInfoView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        inflate(context,R.layout.layout_movie_item,this);
+        inflate(context, R.layout.layout_movie_item, this);
+        coverView = findViewById(R.id.iv_cover);
+        movieName = findViewById(R.id.tv_name);
+        movieScore = findViewById(R.id.tv_score);
+
+    }
+
+
+    public void bindData(String name, String score) {
+        movieName.setText(name);
+        movieScore.setText(score);
     }
 }
