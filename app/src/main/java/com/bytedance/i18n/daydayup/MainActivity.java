64801d66a2a6e87b0c0fc241b7512e516a9fd44e
wangxiaonan <wangxiaonan.master@bytedance.com>
2020-03-19 09:58:49 +0800

    main activity

---
+++ app/src/main/java/com/bytedance/i18n/daydayup/MainActivity.java
@@ -0,0 +1,21 @@
+package com.bytedance.i18n.daydayup;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+/**
+ * @author wangxiaonan
+ */
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
